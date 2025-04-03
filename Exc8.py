af092b4a8193b3a92255d6c8bb89f36e88b23977
RealOmega <nakarin.nokloy@gmail.com>
2025-04-03 21:36:44 +0700

    Update Exc8.py

--- Exc8.py
+++ Exc8.py
@@ -5,6 +5,7 @@ if (UsernameInput == "admin" and PasswordInput == "1111") or (UsernameInput == "
     print("-" * 20, "You can Select", "-" * 20)
     print("1. Vat Calculator")
     print("2. Price Calculator")
+    print("3. Daily Interest")
     userSelected = int(input(">>"))
     if userSelected == 1:
         price = int(input("Price (THB):"))
@@ -15,6 +16,11 @@ if (UsernameInput == "admin" and PasswordInput == "1111") or (UsernameInput == "
         price1 = int(input("First Product Price :"))
         price2 = int(input("Second Product Price :"))
         print(f"Total Price: {price1 + price2} THB")
+    elif userSelected == 3:
+        Loan = int(input("Loan (THB): "))
+        Rate = float(input("Rate (percentage per year): "))
+        daily_interest = (Loan * (Rate / 100)) / 365
+        print(f"Daily Interest: {daily_interest:.2f} THB")
     else:
         print("Invalid selection. Please select 1 or 2.")
 else:
